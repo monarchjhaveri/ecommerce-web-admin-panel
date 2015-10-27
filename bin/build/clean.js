3d5ae4054511aa00500d5eb981828532fff6cfa6
Monarch Wadia <monarchwadia@gmail.com>
2015-10-27 02:05:27 -0400

    New clean command

--- bin/build/clean.js
+++ bin/build/clean.js
@@ -1,4 +1,9 @@
 var fs = require('fs-extra');
 var constants = require('./constants');
 
-fs.remove(constants.filepaths.root.dest);
+fs.remove(constants.filepaths.root.dest + "/*");
+fs.outputFile(constants.filepaths.root.dest + "/.keep", "", function(err) {
+    if (err) {
+        console.log(err)
+    }
+});
