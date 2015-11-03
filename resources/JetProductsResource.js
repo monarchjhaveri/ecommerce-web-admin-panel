d8ccd295c6a0cea1fa4f017b8ce4be1a8a6d9c9d
Monarch Wadia <monarchwadia@gmail.com>
2015-11-03 16:00:31 -0500

    Nicer jet.com failure messages.

--- resources/JetProductsResource.js
+++ resources/JetProductsResource.js
@@ -5,7 +5,7 @@ var JetProductsResource = {};
 JetProductsResource.getProductsList = function(req, res, next) {
     JetService.getProductsList(function(err, data) {
         if (err) {
-            res.status(500).send(_createErrorMessage("getProductsList", err));
+            res.status(500).send(_createErrorMessage("get sku list from Jet.com", err));
         }
 
         res.send(data);
@@ -15,16 +15,16 @@ JetProductsResource.getProductsList = function(req, res, next) {
 JetProductsResource.getDetails = function(req, res, next) {
     JetService.getDetails(req.params.sku, function(err, data) {
         if (err) {
-            res.status(500).send(_createErrorMessage("getDetails", err));
+            res.status(500).send(_createErrorMessage("get sku details from Jet.com", err));
         }
 
         res.send(data);
     })
 };
 
-function _createErrorMessage(functionName, err) {
-    return "Something went wrong while calling [%functionName]. Error Message was: [%errorMessage]"
-        .replace("%functionName", functionName)
+function _createErrorMessage(action, err) {
+    return "Failed to %action: %errorMessage"
+        .replace("%action", action)
         .replace("%errorMessage", err.message);
 }
 
