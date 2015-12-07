2643b854a9eaeae15d868e3f241b73bbefe768a9
Monarch Wadia <monarchwadia@gmail.com>
2015-12-06 20:49:53 -0500

    Added ordering to complete return form.

--- frontend/javascript/components/models/ReturnCompleteModel.js
+++ frontend/javascript/components/models/ReturnCompleteModel.js
@@ -33,54 +33,49 @@ function modelFactory(object){
 }
 
 function optionsFactory(object){
-    var o = {
-        fields: {
-            merchant_order_id: {
-                help: "Test"
-            },
-            items: {
-                item: {
-                    fields: {
-                        order_item_id: {
-                            help: "Test"
-                        }
-                    }
-                }
-            }
-        }
-    };
-    return o;
     return OptionsHelper.applyDefaultOptions({
+        order: ['items', 'merchant_order_id', 'alt_order_id'],
         fields: {
             merchant_order_id: helpBoxOnly("Jet's unique ID for a given merchant order"),
             alt_order_id: helpBoxOnly("Optional merchant supplied order ID"),
             items: {
                 item: {
+                    order: [
+                        'refund_amount',
+                        'total_quantity_returned',
+                        'order_return_refund_qty',
+                        'notes',
+                        'order_item_id',
+                        'alt_order_item_id',
+                        'return_refund_feedback'
+                    ],
                     fields: {
-                        order_item_id: helpBoxOnly("Jet's unique identifier for an item in a merchant order")
+                        order_item_id: helpBoxOnly("Jet's unique identifier for an item in a merchant order"),
+                        alt_order_item_id: helpBoxOnly("Required if returning/refunding an item. If an alt_order_item_id has been associated with the order_item_id via the order accept message, this may be passed instead of the order_item_id field"),
+                        total_quantity_returned: helpBoxOnly("Quantity of the given item that was returned. Logic: Required to indicate the total quantity of units returned for the order item. This quantity should only include the units that were returned. Any cancelled items should not be included if the items were cancelled before the return"),
+                        order_return_refund_qty: helpBoxOnly("Quantity of the given item that was refunded. Logic: Required to indicate the quantity of the given item to be refunded to the customer."),
+                        return_refund_feedback: helpBoxOnly(
+                            "The reason this refund is less than the full amount. \
+                            Valid Values: \
+                                'other' - please give additional information in the notes field\
+                                'item damaged' - should only be used if the reason for the return was not that it was damaged when the customer received it\
+                                'not shipped in original packaging'\
+                                'customer opened item'"
+                        ),
+                        notes: helpBoxOnly("Provide additional information about why the item was refunded for lower than the full amount"),
+                        refund_amount: {
+                            fields: {
+                                principal: helpBoxOnly("Amount to be refunded for the given item in USD associated with the item itself. This should be the total cost for this item not the unit cost"),
+                                tax: helpBoxOnly("Amount to be refunded for the given item in USD associated with tax that was charged for the item"),
+                                shipping_cost: helpBoxOnly("Amount to be refunded for the given item in USD associated with the shipping cost that was allocated to this item"),
+                                shipping_tax: helpBoxOnly("Amount to be refunded for the given item in USD associated with the tax that was charged on shipping")
+                            }
+                        }
                     }
                 }
             },
-            order_item_id: helpBoxOnly("Jet's unique identifier for an item in a merchant order"),
-            alt_order_item_id: helpBoxOnly("Required if returning/refunding an item. If an alt_order_item_id has been associated with the order_item_id via the order accept message, this may be passed instead of the order_item_id field"),
-            total_quantity_returned: helpBoxOnly("Quantity of the given item that was returned. Logic: Required to indicate the total quantity of units returned for the order item. This quantity should only include the units that were returned. Any cancelled items should not be included if the items were cancelled before the return"),
-            order_return_refund_qty: helpBoxOnly("Quantity of the given item that was refunded. Logic: Required to indicate the quantity of the given item to be refunded to the customer."),
-            return_refund_feedback: helpBoxOnly("The reason this refund is less than the full amount. \
-            Valid Values: \
-                'other' - please give additional information in the notes field\
-                'item damaged' - should only be used if the reason for the return was not that it was damaged when the customer received it\
-                'not shipped in original packaging'\
-                    'customer opened item'"
-            ),
-            notes: helpBoxOnly("Provide additional information about why the item was refunded for lower than the full amount"),
-            //refund_amount: {
-            //    fields: {
-            //        principal: helpBoxOnly("Amount to be refunded for the given item in USD associated with the item itself. This should be the total cost for this item not the unit cost"),
-            //        tax: helpBoxOnly("Amount to be refunded for the given item in USD associated with tax that was charged for the item"),
-            //        shipping_cost: helpBoxOnly("Amount to be refunded for the given item in USD associated with the shipping cost that was allocated to this item"),
-            //        shipping_tax: helpBoxOnly("Amount to be refunded for the given item in USD associated with the tax that was charged on shipping")
-            //    }
-            //}
+
+
         }
     });
 }
