c68a659191fd973fb57e3f45904beae569f7763a
Monarch Wadia <monarchwadia@gmail.com>
2015-12-07 23:52:46 -0500

    Requested changes to Category Lookup.

--- frontend/javascript/components/CategoryLookupBox.jsx
+++ frontend/javascript/components/CategoryLookupBox.jsx
@@ -1,5 +1,6 @@
 var React = require("react");
 var CategoryLookupHelper = require("../helpers/CategoryLookupHelper/CategoryLookupHelper");
+var $ = require("jquery");
 
 function _selectBoxFunctionGenerator(self, object) {
     return function() {
@@ -40,6 +41,9 @@ var CategoryLookupBox = React.createClass({ displayName: "CategoryLookupBox",
     clearSelection: function() {
       this.setState({selectedResult: null});
     },
+    useSelectedCategoryId: function() {
+        $("input[name=category_path]")[0].value = this.state.selectedResult.id;
+    },
     render: function() {
         if (this.state.selectedResult) {
             var rows = [];
@@ -50,21 +54,23 @@ var CategoryLookupBox = React.createClass({ displayName: "CategoryLookupBox",
             );
             if (this.state.selectedResult.parent) {
                 var parent = this.state.selectedResult.parent;
-                rows.push(<tr className="clickable" key={this.state.selectedResult.parent.name} onClick={_selectBoxFunctionGenerator(this, parent)}><td>Parent:<br/>{parent.name}</td></tr>);
+                rows.push(<tr key={"parent-header"}><th>Parent</th></tr>);
+                rows.push(<tr className="clickable parent" key={this.state.selectedResult.parent.name} onClick={_selectBoxFunctionGenerator(this, parent)}><td>{parent.name}</td></tr>);
             }
 
             var childRows = [];
             if (this.state.selectedResult.children instanceof Array) {
                 var children = this.state.selectedResult.children;
-                childRows.push(<tr key={"children-header"}><td><h3>Children</h3></td></tr>);
+                childRows.push(<tr key={"children-header"}><th>Children</th></tr>);
                 for (var i = 0; i < children.length; i++) {
-                    childRows.push(<tr className="clickable" key={children[i].id} onClick={_selectBoxFunctionGenerator(this, children[i])}><td>{children[i].name}</td></tr>);
+                    childRows.push(<tr className="clickable children" key={children[i].id} onClick={_selectBoxFunctionGenerator(this, children[i])}><td>{children[i].name}</td></tr>);
                 }
             }
             return (
                 <div className="category-lookup-box">
                     <h2>Category Lookup</h2>
-                    <div className="btn btn-success" onClick={this.clearSelection}>Back to Search</div><br/><br/>
+                    <div className="btn btn-success" onClick={this.clearSelection}>Back to Search</div><br/>
+                    <div className="btn btn-warning" onClick={this.useSelectedCategoryId}>Use This Category ID</div>
                     <table className="table table-hover table-condensed category-result-table">
                         <tbody>
                             {rows}
