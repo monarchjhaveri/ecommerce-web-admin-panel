cff91dbd1c41de9f9a3578650c954f762b6ae061
Monarch <monarchwadia@gmail.com>
2016-04-11 03:56:05 -0400

    Removed references to back end code from frontend.

--- frontend/javascript/Constants.js
+++ frontend/javascript/Constants.js
@@ -1,6 +1,27 @@
-var JET_ORDER_STATUS = require("jet-api").orders.ORDER_STATUS;
-var JET_RETURN_STATUS = require("jet-api").returns.RETURN_STATUS;
-var JET_REFUND_STATUS = require("jet-api").refunds.REFUND_STATUS;
+var JET_ORDER_STATUS = {
+    COMPLETE: "complete",
+      IN_PROGRESS: "inprogress",
+      ACKNOWLEDGED: "acknowledged",
+      READY: "ready",
+      CREATED: "created",
+      DIRECTED_CANCEL: "directedCancel"
+};
+
+var JET_RETURN_STATUS = {
+    JET_REFUNDED: "jetRefunded",
+      COMPLETED_BY_MERCHANT: "completedByMerchant",
+      REFUND_CUSTOMER_WITHOUT_RETURN: "refundCustomerWithoutReturn",
+      ACKNOWLEDGED: "acknowledged",
+      CREATED: "created"
+};
+
+var JET_REFUND_STATUS = {
+    REJECTED_CUSTOMER_CARD_INVALID: "rejected - customer card not valid",
+      REJECTED_CONFLICTS_WITH_RETURN_POLICY: "rejected - refund conflicts with returns policy",
+      ACCEPTED: "accepted",
+      PROCESSING: "processing",
+      CREATED: "created"
+};
 
 var Constants = {
     POPOVER_TYPES: {
